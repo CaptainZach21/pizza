2c01c8629d6c205051908c2a86ade14b18283dab
kpaul808 <kainoap808@gmail.com>
2016-06-29 11:36:27 -1000

    added 3 buttons

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var Toppings = ["Pepperoni", "Mushroom", "Sausage", "Bell Pepper", 
-"Kim Chee", "Pineapple", "Spinach", "Garlic", "Kalua Pig", "Olive"];
+"Kim Chee", "Pineapple", "Spinach", "Garlic", "Kalua Pig", "Olive", "Canadian Bacon", "Bacon"];
 
-function pickTopping(){
+function pickTopping2(){
 	var toppingNumber = Math.floor(Math.random()*Toppings.length);
 	var toppingNumber2 = Math.floor(Math.random()*Toppings.length);
 	document.getElementById('pizza').innerHTML = "<h1>"+Toppings[toppingNumber]+" and "+
@@ -9,4 +9,18 @@ function pickTopping(){
 	console.log(Toppings[toppingNumber]);
 	console.log(toppingNumber);
 }
-
+function pickTopping1(){
+	var toppingNumber = Math.floor(Math.random()*Toppings.length);
+	document.getElementById('pizza').innerHTML = "<h1>"+Toppings[toppingNumber]+"</h1>";
+	console.log(Toppings[toppingNumber]);
+	console.log(toppingNumber);	
+}
+function pickTopping3(){
+	var toppingNumber = Math.floor(Math.random()*Toppings.length);
+	var toppingNumber2 = Math.floor(Math.random()*Toppings.length);
+	var toppingNumber3 = Math.floor(Math.random()*Toppings.length);
+	document.getElementById('pizza').innerHTML = "<h1>"+Toppings[toppingNumber]+", "+
+	Toppings[toppingNumber2]+" and "+Toppings[toppingNumber3]+"</h1>";
+	console.log(Toppings[toppingNumber]);
+	console.log(toppingNumber);
+}
